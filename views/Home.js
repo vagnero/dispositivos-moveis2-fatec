929f272924125c6a27a77a02eafc286a822f05fd
Audrey Bergamine <audreybergamini@outlook.com>
2024-10-05 10:18:20 -0300

    .

--- views/Home.js
+++ views/Home.js
@@ -131,7 +131,7 @@ const Home = () => {
     },
     
     buttonPressed: {
-      backgroundColor: colors.primary,
+      backgroundColor: colors.wineCardBackground, 
     },
 
     textButtonPressed: {
