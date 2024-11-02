63b8c1abdbecc5642d328e123158378a2b08e2e4
vagnero <vagnerimperador16@gmail.com>
2024-11-02 11:41:37 -0300

    ícone de favorito consertado

--- components/ItemCard.js
+++ components/ItemCard.js
@@ -3,42 +3,91 @@ import { ThemeContext } from '../context/ThemeContext';
 import { View, Text, Image, TouchableOpacity, Platform } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import dbContext from '../context/dbContext';
-import Items from './Items';
+import { FontAwesome } from '@expo/vector-icons';
+import { useUser } from '../context/UserContext';
+import AlertModal from '../components/AlertModal';
 
-const ItemCard = ({ item, onPressAddToCart, updateCartItems }) => {
+const ItemCard = ({ item, onPressAddToCart, onToggleFavorite }) => {
   const navigation = useNavigation();
   const { colors } = useContext(ThemeContext);
-  const [soldCount, setSoldCount] = useState(0); // Inicializa como 0
-  const [totalQuantity, setTotalQuantity] = useState(0); // Estado para manter a quantidade total
+  const [soldCount, setSoldCount] = useState(0);
+  const [modalAlertVisible, setModalAlertVisible] = useState(false);
+  const [mensagem, setMensagem] = useState('');
+  const { currentUser } = useUser();
+  const [isFavorite, setIsFavorite] = useState(false);
 
   useEffect(() => {
     const fetchItems = async () => {
       try {
-        // Carregar os itens do dbContext
         const itemsArray = dbContext.data.items;
-
-        // Encontrar o item correspondente pelo itemName
         const storedItem = itemsArray.find(i => i.itemName === item.itemName);
-
-        if (storedItem) {
-          setSoldCount(storedItem.itemSold);
-        } else {
-          const matchingItem = Items.find(i => i.itemName === item.itemName);
-          setSoldCount(matchingItem ? matchingItem.itemSold || 0 : 0);
-        }
+        setSoldCount(storedItem ? storedItem.itemSold : 0);
       } catch (error) {
         console.error('Erro ao carregar items do Firestore:', error);
       }
     };
 
+    const checkIfFavorite = async () => {
+      if (!currentUser || !currentUser.nome) return;
+
+      const itemId = `${item.itemName}_${currentUser.nome}`;
+      const favoriteItems = dbContext.getAll('favoriteItems');
+      const isFav = favoriteItems.some(fav => fav.id === itemId);
+      setIsFavorite(isFav);
+    };
+
     fetchItems();
-  }, [item.itemName]);
+    checkIfFavorite();
+  }, [item.itemName, currentUser]);
+
+  const toggleHeart = async () => {
+    if (!currentUser || !currentUser.nome) {
+      setMensagem('Para favoritar é necessário estar logado');
+      setModalAlertVisible(true);
+      return;
+    }
+
+    if (isFavorite) {
+      await removeFavoriteItem(item.itemName);
+      setIsFavorite(false); // Atualiza o estado local
+    } else {
+      await saveFavoriteItem(item);
+      setIsFavorite(true); // Atualiza o estado local
+    }
+
+    onToggleFavorite(item); // Chama a função para atualizar a lista de favoritos no componente pai
+  };
+
+  const saveFavoriteItem = async (item) => {
+    if (!currentUser || !currentUser.nome) return;
+
+    const favoriteItem = {
+      itemName: item.itemName,
+      id: `${item.itemName}_${currentUser.nome}`,
+    };
+
+    try {
+      await dbContext.addItem('favoriteItems', favoriteItem);
+      console.log('Item favorito salvo:', favoriteItem);
+    } catch (error) {
+      console.error('Erro ao salvar item favorito:', error);
+    }
+  };
+
+  const removeFavoriteItem = async (itemName) => {
+    if (!currentUser || !currentUser.nome) return;
+
+    try {
+      const itemId = `${itemName}_${currentUser.nome}`;
+      await dbContext.removeItem('favoriteItems', itemId);
+      console.log('Item removido dos favoritos:', itemName);
+    } catch (error) {
+      console.error('Erro ao remover item favorito:', error);
+    }
+  };
 
   const handleAddToCart = () => {
-    const newTotalQuantity = totalQuantity + 1; // Incrementa o total
-    setTotalQuantity(newTotalQuantity); // Atualiza o estado com o novo total
-    // console.log(`Total de itens no carrinho: ${newTotalQuantity}`);
-    onPressAddToCart({ ...item, imageSource: item.imageSource, quantity: 1 }, updateCartItems); // Passa 1 como quantidade do item adicionado
+    onPressAddToCart({ ...item, imageSource: item.imageSource, quantity: 1 });
   };
 
   const styles = {
@@ -72,8 +121,6 @@ const ItemCard = ({ item, onPressAddToCart, updateCartItems }) => {
     div_image_item: {
       width: 50,
       height: 120,
-      // width: 40,
-      // height: 70,
     },
     div_text_item: {
       width: '90%',
@@ -96,21 +143,27 @@ const ItemCard = ({ item, onPressAddToCart, updateCartItems }) => {
       fontWeight: 'bold',
     },
     addButton: {
-      marginLeft: 10, // Adicionando margem para separar o botão do preço
+      marginLeft: 10,
     },
   };
 
   return (
     <View style={styles.div_item}>
-      <Text style={styles.div_text_item} onPress={() => navigation.navigate('Sobre', item)}>{item.itemName}</Text>
+      <Text style={styles.div_text_item} onPress={() => navigation.navigate('Sobre', item)}>
+        {item.itemName}
+      </Text>
       <TouchableOpacity onPress={() => navigation.navigate('Sobre', item)}>
         <View style={styles.div_image_text_item}>
           <Image source={item.imageSource} style={styles.div_image_item} />
           <View>
-            <Text>Vendidos: {soldCount}</Text>
-            <Text>Rate: {item.itemSigns}
-              <Image source={require('../assets/info/signs.png')}/>
-            </Text>
+            <TouchableOpacity onPress={toggleHeart}>
+              <FontAwesome
+                name={isFavorite ? 'heart' : 'heart-o'}
+                size={20}
+                color="red"
+              />
+            </TouchableOpacity>
+            <Text>Rate: {item.itemSigns}</Text>
           </View>
         </View>
       </TouchableOpacity>
@@ -120,6 +173,11 @@ const ItemCard = ({ item, onPressAddToCart, updateCartItems }) => {
           <Image source={require('../assets/carrinho/plus.png')} style={{ marginBottom: 10 }} />
         </TouchableOpacity>
       </View>
+      <AlertModal
+        visible={modalAlertVisible}
+        message={mensagem}
+        onClose={() => setModalAlertVisible(false)}
+      />
     </View>
   );
 };
